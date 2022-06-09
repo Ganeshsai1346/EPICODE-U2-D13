ea1c985f937a28e882a94a90734b54ed2c968410
Ganesh sai <ganeshsqai1346@gmail.com>
2022-06-09 16:18:32 +0200

    Completed some exercises

---
+++ index.js
@@ -0,0 +1,64 @@
+/** @format */
+//1 & //2
+const grid = document.querySelector(".container > .row");
+const displayBooks = async () => {
+  const response = await fetch("https://striveschool-api.herokuapp.com/books");
+  const books = await response.json();
+
+  books.forEach((book) => {
+    const col = document.createElement("div");
+    col.className = "col-md-3";
+
+    col.innerHTML = `<div class="single-album card mb-4 shadow-sm">
+                        <img src= ${book.img} class="card-img-top" alt="${book.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${book.title}<h5>
+                            <p class="card-text">
+                                ${book.category}
+                            </p>
+                            <div
+                                class="d-flex justify-content-between align-items-center"
+                            >
+                                <div class="btn-group">
+                                    <button
+                                        type="button"
+                                        id="cartButton"
+                                        class="btn btn-outline-secondary"
+                                    >
+                                        Add to Cart
+                                    </button>
+                                    <button
+                                        type="button"
+                                        class="btn btn-outline-secondary"
+                                        onclick="removeCard(event)"
+                                    >
+                                        skip
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+    grid.appendChild(col);
+  });
+};
+
+displayBooks();
+
+//5.
+const removeCard = (e) => {
+  e.target.closest(".col-md-3").remove();
+};
+
+//4. When this button is pressed: 1) add the item to another list (the cart), and 2) change the card styling to show that the element is in the cart (eg. red border, a badge, an icon… you choose)
+
+const addToCart = (e) => {
+  let cards = document.querySelectorAll(".col-md-3");
+
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].onclick = (e) => {
+      e.target.classList.add("addBorder");
+    };
+  }
+};
+const btn = document.querySelector("#cartButton");
+btn.addEventListener("click", addToCart);
